885933542798e9f0fb81dfb452cd04e0b061869d
Matjaž Grosek <matjaz@designmadness.si>
2013-09-25 17:42:22 +0200

    Removed commented code

--- comments.js
+++ comments.js
@@ -14,19 +14,8 @@ var Db = require('mongodb').Db,
     BSON = require('mongodb').pure().BSON,
     assert = require('assert');
 
-//var moment = require('moment');
-
-
 // Open the connection to the server
  var db = new Db('local', new Server('localhost', 27017));
-  // Establish connection to db
-  // var comments;
-  // db.open(function(err, db) {
-  //   assert.equal(null, err);
-  //   comments = { hello: 'world' };
-  //   //db.on('close', test.done.bind(test));
-  //   db.close();
-  // });
 
 server.listen(8080);
 var express=require('express');
@@ -42,15 +31,10 @@ app.get('/', function (req, res) {
 });
 
 app.post('/addComment', function(req, res){
-  var obj = {};
-  //console.log('body: ' + JSON.stringify(req.body));
- // res.send(req.body);
   db.open(function(err, db) {
     assert.equal(null, err);
     db.collection('iptvbeats', function(err, collection) {
       collection.ensureIndex({"_id":1}, {unique:true}, function(err, indexName) {
-      //   console.log(err);
-       //  console.log(indexName);
         collection.insert({category_id: req.body.category_id, user_id: req.body.user_id, comment: req.body.comment, date_time: new Date()});
         db.close();
         res.send(req.body);
@@ -76,11 +60,8 @@ io.sockets.on('connection', function (socket) {
     socket.emit('comments', getComments(socket));
   });
 
-  //socket.emit('comments', { hello: 'world' });
-  // socket.on('my other event', function (data) {
-  //   console.log(data);
-  // });
 });
+
 function custom_sort(a, b) {
     return new Date(a.date_time).getTime() - new Date(b.date_time).getTime();
 }
@@ -89,9 +70,6 @@ function getComments(socket) {
     db.open(function(err, db) {
       assert.equal(null, err);
       db.collection('iptvbeats', function(err, collection) {
-        // var collection_ = collection;
-        var comments_ = Array();
-
         collection.group(
             { "category_id": true },
             {"category_id":  { $exists: true}},
@@ -101,14 +79,14 @@ function getComments(socket) {
             },
             function(item, summaries){
               summaries.count++;
-              //if(item.category_id != null)
               summaries.comments[summaries.comments.length] = item;
             },
-            true,  // use the group command
-            function(err, results){ //self.eventEmitter.emit(doneEvent, results)
-              if(results.comments !== undefined) 
-                 console.log(results.comments)
-              //.sort(custom_sort);
+            true,  
+            function(err, results){ 
+              //TODO: sort comments
+              //if(results.comments !== undefined) 
+              //  console.log(results.comments)
+              
               socket.emit('comments', results);
               db.close();
             }
