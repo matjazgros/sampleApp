e9bfc26c957a6e95ee999400e0b78d2c23e2ada2
Matjaž Grosek <matjaz@designmadness.si>
2013-09-28 15:47:06 +0200

    Updating comments after new comment

--- comments.js
+++ comments.js
@@ -3,7 +3,7 @@ var app = require('express')(),
     io = require('socket.io').listen(server); 
 
 var mongodb = require('mongodb');
-var uri = process.env.DATABASE_URL;
+var uri = 'mongodb://iptvbeat:iptvbeat@ds063307.mongolab.com:63307/iptvbeat';//process.env.DATABASE_URL;
 var baza;
 mongodb.Db.connect(uri, { server: { auto_reconnect: true } }, function (err, db) {
       baza = db;
@@ -12,10 +12,10 @@ var port = process.env.PORT || 5000;
 server.listen(port);
 var express=require('express');
 app.configure(function () {
-    app.use(express.cookieParser());
-    app.use(express.bodyParser());
-    app.use('/js', express.static(__dirname + '/js'));
-    app.use(app.router);    
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use('/js', express.static(__dirname + '/js'));
+  app.use(app.router);    
 });
 
 app.get('/', function (req, res) {
@@ -24,31 +24,23 @@ app.get('/', function (req, res) {
 //add comment to database
 app.post('/addComment', function(req, res) {
   try {
-      baza.collection('iptvbeat', function(err, collection) {
-        collection.ensureIndex({"_id":1}, {unique:true}, function(err, indexName) {
-          collection.insert({category_id: req.body.category_id, user_id: req.body.user_id, comment: req.body.comment, date_time: new Date()}, function(err, db) {
-            res.send({successful: true});
-          });
+    baza.collection('iptvbeat', function(err, collection) {
+      collection.ensureIndex({"_id":1}, {unique:true}, function(err, indexName) {
+        collection.insert({category_id: req.body.category_id, user_id: req.body.user_id, comment: req.body.comment, date_time: new Date()}, function(err, db) {
+          res.send({successful: true});
         });
       });
+    });
   } catch(e) {
     res.send({successful: false});
   }    
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('comments', getComments(socket));
-  var commentsTimer = setInterval(function () {
-    getComments(socket);
-  }, 1000);
-
-  socket.on('disconnect', function () {
-    clearInterval(commentsTimer);
-    //baza.close();
-  });
-
+  updateComments(socket);
+  
   socket.on('addedComment', function () {
-    io.sockets.emit('comments', getComments(socket));
+    updateComments(socket);    
   });
 
 });
@@ -62,34 +54,32 @@ function sortByDate(a, b) {
     return new Date(a.date_time).getTime() - new Date(b.date_time).getTime();
 }
 
-function getComments(socket) {
+function updateComments(socket) {
   try {
-
-      baza.collection('iptvbeat', function(err, collection) {
-        collection.group(
-            { "category_id": true },
-            { "category_id": { $exists: true}},
-            {
-              count: 0,
-              comments: []
-            },
-            function(item, summaries){
-              summaries.count++;
-              summaries.comments[summaries.comments.length] = item;
-            },
-            true,  
-            function(err, results){ 
-                //sort comments
-                for (var i in results) {
-                  category = results[i];
-                  category.comments.sort(sortByDate);
-                }
-              
-              socket.emit('comments', results);
+    baza.collection('iptvbeat', function(err, collection) {
+      collection.group(
+          { "category_id": true },
+          { "category_id": { $exists: true}},
+          {
+            count: 0,
+            comments: []
+          },
+          function(item, summaries){
+            summaries.count++;
+            summaries.comments[summaries.comments.length] = item;
+          },
+          true,  
+          function(err, results){ 
+            //sort comments
+            for (var i in results) {
+              category = results[i];
+              category.comments.sort(sortByDate);
             }
-        );
-      });
+            io.sockets.emit('comments', results);
+          }
+      );
+    });
   } catch(e) {
-    getComments(socket);
+    //updateComments(socket);
   }    
 }
